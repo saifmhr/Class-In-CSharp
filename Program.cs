822c272dbf80034b8e75393afbdccbd3d1bccd1b
saifmhr <saifmahar165@gmail.com>
2022-10-21 08:17:57 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+class Customer
+{
+    string _Firstname;
+    string _Lastname;
+
+
+    public Customer() : this("no one firstname","no one lastname") { }
+    public Customer(string Firstname, string lastname)
+    { 
+     this._Firstname = Firstname;
+        this._Lastname = lastname;
+    }
+
+    public void PrintFullName()
+    {
+        Console.WriteLine("Customer full name = {0}", this._Firstname + " " + this._Lastname);
+    }
+
+    ~Customer()
+    {
+        //Clean code
+    }
+}
+
+ class Program
+    {
+        static void Main()
+        {
+
+     //   Customer C1 = new Customer("Saifullah", "Mahar");
+     Customer C1 = new Customer();
+        C1.PrintFullName();
+
+        Customer C2 = new Customer("saifullah","Mahar");
+        C2.PrintFullName();
+        }
+    }
